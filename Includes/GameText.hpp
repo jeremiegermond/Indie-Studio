eee857ab38353933dfd6c8c553a2581d4fe45cef
Marine Poteau <marinepoteau@MacBook-Pro-de-Marine.local>
2022-05-27 11:26:26 +0200

    [+] add Game Text

---
+++ Includes/GameText.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2022
+** GameText.hpp
+** File description:
+** GameText
+*/
+
+#ifndef GAMETEXT_HPP_
+#define GAMETEXT_HPP_
+
+#include "IEntity.hpp"
+
+namespace bomberman {
+    class GameText : public IEntity {
+        public:
+            GameText();
+            ~GameText();
+
+        protected:
+        private:
+    };
+}
+
+#endif /* !GAMETEXT_HPP_ */
