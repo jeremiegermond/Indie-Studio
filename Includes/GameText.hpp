2115e79ba1227da662fd67244c89e8cbd077a69e
Marine Poteau <marinepoteau@MacBook-Pro-de-Marine.local>
2022-05-31 15:34:46 +0200

    [+] add title menu and GameText in iEntity

--- Includes/GameText.hpp
+++ Includes/GameText.hpp
@@ -13,12 +13,23 @@
 namespace bomberman {
     class GameText : public IEntity {
         private:
-            Font font;
+            Font _font;
+            std::string _msg;
+            Vector2 _fontPosition;
+            float _fontSize;
+            bool _flash;
+            Color _color;
+            bool active;
 
         public:
-            GameText(Font font);
-            ~GameText();
-            void LinkFontText();
+            GameText(const std::string &font, const std::string &msg, float x, float y, float fontSize, Color color);
+            ~GameText() override;
+            void Draw();
+
+            void Reset() override;
+            void Update() override;
+            void SetActive(bool activate) override;
+            void Move(Vector3 velocity) override;
     };
 }
 
