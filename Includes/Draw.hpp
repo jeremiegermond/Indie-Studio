85bc29caa4297c9d057f5737c01015600794cb86
Jeremie <jeremie@localhost.localdomain>
2022-06-02 19:42:32 +0200

    [+] draw encapsulation functions

---
+++ Includes/Draw.hpp
@@ -0,0 +1,65 @@
+/*
+** EPITECH PROJECT, 2022
+** Draw.hpp
+** File description:
+** TODO
+*/
+
+#pragma once
+
+#include "raylib.h"
+#include <functional>
+#include <iostream>
+
+namespace bomberman {
+    class Draw {
+    public:
+        // Set background color (framebuffer clear color)
+        inline void clearBackground(Color color);
+
+        // Setup canvas (framebuffer) to start drawing
+        inline void beginDrawing(void);
+
+        // End canvas drawing and swap buffers (double buffering)
+        inline void endDrawing(void);
+
+        // Begin 2D mode with custom camera (2D)
+        inline void beginMode2D(Camera2D camera);
+
+        // Ends 2D mode with custom camera
+        inline void endMode2D(void);
+
+        // Begin 3D mode with custom camera (3D)
+        inline void beginMode3D(Camera3D camera);
+
+        // Ends 3D mode and returns to default 2D orthographic mode
+        inline void endMode3D(void);
+
+        // Begin drawing to render texture
+        inline void beginTextureMode(RenderTexture2D target);
+
+        // Ends drawing to render texture
+        inline void endTextureMode(void);
+
+        // Begin custom shader drawing
+        inline void beginShaderMode(Shader shader);
+
+        // End custom shader drawing (use default shader)
+        inline void endShaderMode(void);
+
+        //additional function: draw
+        static inline void draw(const std::function<void()> &function);
+
+        //additional function: mode2D
+        static inline void mode2D(const Camera2D &camera, const std::function<void()> &function);
+
+        //additional function: mode3D
+        static inline void mode3D(const Camera3D &camera, const std::function<void()> &function);
+
+        //additional function: textureMode
+        static inline void textureMode(const RenderTexture2D &texture, const std::function<void()> &function);
+
+        //additional function: shaderMode
+        static inline void shaderMode(const Shader &shader, const std::function<void()> &function);
+    };
+}
---
+++ Src/Encapsulation/Draw.cpp
@@ -0,0 +1,84 @@
+/*
+** EPITECH PROJECT, 2022
+** Draw.cpp
+** File description:
+** TODO
+*/
+
+#include "Draw.hpp"
+
+namespace bomberman {
+    inline void clearBackground(const Color &color) {
+        return ClearBackground(color);
+    }
+    
+    inline void beginDrawing(void) {
+        return BeginDrawing();
+    }
+    
+    inline void endDrawing(void) {
+        return EndDrawing();
+    }
+    
+    inline void beginMode2D(const Camera2D &camera) {
+        return BeginMode2D(camera);
+    }
+    
+    inline void endMode2D(void) {
+        return EndMode2D();
+    }
+    
+    inline void beginMode3D(const Camera3D &camera) {
+        return BeginMode3D(camera);
+    }
+    
+    inline void endMode3D(void) {
+        return EndMode3D();
+    }
+    
+    inline void beginTextureMode(const RenderTexture2D &target) {
+        return BeginTextureMode(target);
+    }
+    
+    inline void endTextureMode(void) {
+        return EndTextureMode();
+    }
+    
+    inline void beginShaderMode(const Shader &shader) {
+        return BeginShaderMode(shader);
+    }
+    
+    inline void endShaderMode(void) {
+        return EndShaderMode();
+    }
+
+    static inline void draw(const std::function<void()> &function) {
+        beginDrawing();
+        function();
+        endDrawing();
+    }
+
+    static inline void mode2D(const Camera2D &camera, const std::function<void()> &function) {
+        beginMode2D(camera);
+        function();
+        endMode2D();
+    }
+
+    static inline void mode3D(const Camera3D &camera, const std::function<void()> &function) {
+        beginMode3D(camera);
+        function();
+        endMode3D();
+    }
+
+    static inline void textureMode(const RenderTexture2D &texture, const std::function<void()> &function) {
+        beginTextureMode(texture);
+        function();
+        endTextureMode();
+    }
+
+    static inline void shaderMode(const Shader &shader, const std::function<void()> &function) {
+        beginShaderMode(shader);
+        function();
+        endShaderMode();
+    }
+}
