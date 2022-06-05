9fefd2dbabedb82b868aa5ed1956555c0c17cc96
Jeremie <jeremie@localhost.localdomain>
2022-06-05 16:16:56 +0200

    [+] all encapsulated models functions

--- Includes/Encapsulation/Models.hpp
+++ Includes/Encapsulation/Models.hpp
@@ -13,71 +13,71 @@ namespace bomberman {
     class Models {
     public:
         // Basic geometric 3D shapes drawing functions
-        Models();
-        ~Models();
+        Models() {}
+        ~Models() {}
 
         // Draw a line in 3D world space
-        inline void drawLine3D(MyVector3 startPos, MyVector3 endPos, MyColor color);
+        inline void drawLine3D(const MyVector3 &startPos, const MyVector3 &endPos, const MyColor &color);
 
         // Draw a point in 3D space, actually a small line
-        inline void drawPoint3D(MyVector3 position, MyColor color);
+        inline void drawPoint3D(const MyVector3 &position, const MyColor &color);
 
         // Draw a circle in 3D world space
-        inline void drawCircle3D(MyVector3 center, float radius, MyVector3 rotationAxis, float rotationAngle, MyColor color);
+        inline void drawCircle3D(const MyVector3 &center, const float &radius, const MyVector3 &rotationAxis, const float &rotationAngle, const MyColor &color);
 
         // Draw a color-filled triangle (vertex in counter-clockwise order!)
-        inline void drawTriangle3D(MyVector3 v1, MyVector3 v2, MyVector3 v3, MyColor color);
+        inline void drawTriangle3D(const MyVector3 &v1, const MyVector3 &v2, const MyVector3 &v3, const MyColor &color);
 
         // Draw a triangle strip defined by points
-        inline void drawTriangleStrip3D(MyVector3 *points, int pointsCount, MyColor color);
+        inline void drawTriangleStrip3D(MyVector3 *points, const int &pointsCount, const MyColor &color);
 
         // Draw cube
-        inline void drawCube(MyVector3 position, float width, float height, float length, MyColor color);
+        inline void drawCube(const MyVector3 &position, const float &width, const float &height, const float &length, const MyColor &color);
 
         // Draw cube (Vector version)
-        inline void drawCubeV(MyVector3 position, MyVector3 size, MyColor color);
+        inline void drawCubeV(const MyVector3 &position, const MyVector3 &size, const MyColor &color);
 
         // Draw cube wires
-        inline void drawCubeWires(MyVector3 position, float width, float height, float length, MyColor color);
+        inline void drawCubeWires(const MyVector3 &position, const float &width, const float &height, const float &length, const MyColor &color);
 
         // Draw cube wires (Vector version)
-        inline void drawCubeWiresV(MyVector3 position, MyVector3 size, MyColor color);
+        inline void drawCubeWiresV(const MyVector3 &position, const MyVector3 &size, const MyColor &color);
 
         // Draw cube textured
-        inline void drawCubeTexture(MyTexture texture, MyVector3 position, float width, float height, float length, MyColor color);
+        inline void drawCubeTexture(const MyTexture &texture, const MyVector3 &position, const float &width, const float &height, const float &length, const MyColor &color);
 
         // Draw cube with a region of a texture
-        inline void drawCubeTextureRec(MyTexture texture, MyRectangle source, MyVector3 position, float width, float height, float length, MyColor color);
+        inline void drawCubeTextureRec(const MyTexture &texture, const MyRectangle &source, const MyVector3 &position, const float &width, const float &height, const float &length, const MyColor &color);
 
         // Draw sphere
-        inline void drawSphere(MyVector3 centerPos, float radius, MyColor color);
+        inline void drawSphere(const MyVector3 &centerPos, const float &radius, const MyColor &color);
 
         // Draw sphere with extended parameters
-        inline void drawSphereEx(MyVector3 centerPos, float radius, int rings, int slices, MyColor color);
+        inline void drawSphereEx(const MyVector3 &centerPos, const float &radius, const int &rings, const int &slices, const MyColor &color);
 
         // Draw sphere wires
-        inline void drawSphereWires(MyVector3 centerPos, float radius, int rings, int slices, MyColor color);
+        inline void drawSphereWires(const MyVector3 &centerPos, const float &radius, const int &rings, const int &slices, const MyColor &color);
 
         // Draw a cylinder/cone
-        inline void drawCylinder(MyVector3 position, float radiusTop, float radiusBottom, float height, int slices, MyColor color);
+        inline void drawCylinder(const MyVector3 &position, const float &radiusTop, const float &radiusBottom, const float &height, const int &slices, const MyColor &color);
 
         // Draw a cylinder with base at startPos and top at endPos
-        inline void drawCylinderEx(MyVector3 startPos, MyVector3 endPos, float startRadius, float endRadius, int sides, MyColor color);
+        inline void drawCylinderEx(const MyVector3 &startPos, const MyVector3 &endPos, const float &startRadius, const float &endRadius, const int &sides, const MyColor &color);
 
         // Draw a cylinder/cone wires
-        inline void drawCylinderWires(MyVector3 position, float radiusTop, float radiusBottom, float height, int slices, MyColor color);
+        inline void drawCylinderWires(const MyVector3 &position, const float &radiusTop, const float &radiusBottom, const float &height, const int &slices, const MyColor &color);
 
         // Draw a cylinder wires with base at startPos and top at endPos
-        inline void drawCylinderWiresEx(MyVector3 startPos, MyVector3 endPos, float startRadius, float endRadius, int sides, MyColor color);
+        inline void drawCylinderWiresEx(const MyVector3 &startPos, const MyVector3 &endPos, const float &startRadius, const float &endRadius, const int &sides, const MyColor &color);
 
         // Draw a plane XZ
-        inline void drawPlane(MyVector3 centerPos, MyVector2 size, MyColor color);
+        inline void drawPlane(const MyVector3 &centerPos, const MyVector2 &size, const MyColor &color);
 
         // Draw a ray line
-        inline void drawRay(MyRay ray, MyColor color);
+        inline void drawRay(const MyRay &ray, const MyColor &color);
 
         // Draw a grid (centered at (0, 0, 0))
-        inline void drawGrid(int slices, float spacing);
+        inline void drawGrid(const int &slices, const float &spacing);
     };
 }
 
