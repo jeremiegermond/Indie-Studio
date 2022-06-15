559f6247adf67dc7c459aa2477cc0c2fe0568ded
Jeremie <jeremie@localhost.localdomain>
2022-06-15 15:16:18 +0200

    [+] gamepad encapsulated

---
+++ Includes/Encapsulation/MyGamepad.hpp
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2022
+** MyGamepad.hpp
+** File description:
+** TODO
+*/
+
+#ifndef BOMBERMAN_MYGAMEPAD_HPP
+#define BOMBERMAN_MYGAMEPAD_HPP
+
+#include "raylib.h"
+
+namespace bomberman {
+    class MyGamepad {
+    public:
+        // Check if a gamepad is available
+        static bool isGamepadAvailable(int gamepad);
+
+        // Check if a gamepad button is being pressed
+        static bool isGamepadButtonDown(int gamepad, int button);
+
+        // Get axis movement value for a gamepad axis
+        static float getGamepadAxisMovement(int gamepad, int axis);
+
+        // Check if a gamepad button has been pressed once
+        static bool isGamepadButtonPressed(int gamepad, int button);
+    };
+}
+
+#endif //BOMBERMAN_MYGAMEPAD_HPP
---
+++ Src/Encapsulation/Gamepad.cpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2022
+** Gamepad.cpp
+** File description:
+** TODO
+*/
+
+#include "MyGamepad.hpp"
+
+namespace bomberman {
+    bool MyGamepad::isGamepadAvailable(int gamepad) {
+        return IsGamepadAvailable(gamepad);
+    }
+
+    bool MyGamepad::isGamepadButtonDown(int gamepad, int button) {
+        return IsGamepadButtonDown(gamepad, button);
+    }
+
+    float MyGamepad::getGamepadAxisMovement(int gamepad, int axis) {
+        return GetGamepadAxisMovement(gamepad, axis);
+    }
+
+    bool MyGamepad::isGamepadButtonPressed(int gamepad, int button) {
+        return IsGamepadButtonPressed(gamepad, button);
+    }
+}
